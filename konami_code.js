6aa62bae8f67cdf6f11703fd13a20181b4053ef8
louis baptiste <louisvis@ymail.com>
2020-03-30 03:10:54 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,19 +1,23 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
-function init(e) {
+function init() {
   let index = 0;
-  const key = parseInt(e.detail || e.which);
 
-    if (key === code[index]) {
+  document.body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.which || e.detail);
+
+    if (code[index] === key) {
       index++;
 
       if (index === code.length) {
-        alert("Hurray!");
+        alert('Hurray!');
 
         index = 0;
       }
     } else {
       index = 0;
     }
-  }
+  });
 }
+
+init();
